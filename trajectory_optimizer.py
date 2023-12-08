442816cc6f8ec9e3397498a9f584952c23d0891d
ctl009 <chris.ts.lee@gmail.com>
2023-12-08 17:08:52 -0500

    minor changes

--- trajectory_optimizer.py
+++ trajectory_optimizer.py
@@ -135,7 +135,9 @@ class TrajectoryOptimizer:
         u = [np.zeros((2,))]
         t = [t0]
 
-        while np.linalg.norm(np.array(x[-1][0:2]) - xf[0:2]) > 1e-3 and t[-1] < tf and iter < max_iter:
+        i = 0
+
+        while np.linalg.norm(np.array(x[-1][0:2]) - xf[0:2]) > 1e-3 and t[-1] < tf and i < max_iter:
             current_time = t[-1]
             current_x = x[-1]
             current_u_command = np.zeros(2)
@@ -154,6 +156,8 @@ class TrajectoryOptimizer:
             t.append(t[-1] + dt)
             x.append(sol.y[:, -1])
             u.append(current_u_command)
+
+            i += 1
 
         x = np.array(x)
         u = np.array(u)
