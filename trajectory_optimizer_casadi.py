4504bd462f135ba8936636fe3f11e0be4affed51
ctl009 <chris.ts.lee@gmail.com>
2023-12-10 16:35:45 -0500

    Added signed distance field

--- trajectory_optimizer_casadi.py
+++ trajectory_optimizer_casadi.py
@@ -232,21 +232,21 @@ def optimize_trajectory(quadrotor, obstacles, N, dt, initial_trajectory, alpha, 
             opti.subject_to(opti.bounded(input_min - u_f, uk[0, i], input_max - u_f))
 
     # Add boundary constraints
-    # xmin, ymin, xmax, ymax = boundary
+    xmin, ymin, xmax, ymax = boundary
     print("boundary =", boundary)
 
-    # for k in range(N):
-    #     # Extract the position state at timestep k
-    #     xk = X[k, 0]
-    #     yk = X[k, 1]
+    for k in range(N):
+        # Extract the position state at timestep k
+        xk = X[k, 0]
+        yk = X[k, 1]
 
-    #     # Add boundary constraints
-    #     opti.subject_to(opti.bounded(xmin, xk, xmax))  # x-coordinate must be within boundaries
-    #     opti.subject_to(opti.bounded(ymin, yk, ymax))  # y-coordinate must be within boundaries
+        # Add boundary constraints
+        opti.subject_to(opti.bounded(xmin, xk, xmax))  # x-coordinate must be within boundaries
+        opti.subject_to(opti.bounded(ymin, yk, ymax))  # y-coordinate must be within boundaries
 
     # Add top box obstacle constraints
     box = boxes[0]
-    # xmin, ymin, xmax, ymax = box
+    xmin, ymin, xmax, ymax = box
     print("box =", box)
     # Define the margins around the box where the quadrotor should not enter
     # margin = 0  # Distance margin
@@ -280,20 +280,20 @@ def optimize_trajectory(quadrotor, obstacles, N, dt, initial_trajectory, alpha, 
     #     penalty += inside_box * ((xk - xmin)**2 + (xk - xmax)**2 + (yk - ymin)**2 + (yk - ymax)**2)
 
     # Obstacle barrier function constraint
-    # epsilon = 1e-3  # Small offset to prevent the log from blowing up
-    # barrier = 0
-    # for k in range(N):
-    #     xk, yk = X[k, 0], X[k, 1]
-    #     barrier += -ca.log(xk - xmin + epsilon)  # Barrier for left edge
-    #     barrier += -ca.log(xmax - xk + epsilon)  # Barrier for right edge
-    #     barrier += -ca.log(yk - ymin + epsilon)  # Barrier for bottom edge
-    #     barrier += -ca.log(ymax - yk + epsilon)  # Barrier for top edge
+    epsilon = 1e-3  # Small offset to prevent the log from blowing up
+    barrier = 0
+    for k in range(N):
+        xk, yk = X[k, 0], X[k, 1]
+        barrier += -ca.log(xk - xmin + epsilon)  # Barrier for left edge
+        barrier += -ca.log(xmax - xk + epsilon)  # Barrier for right edge
+        barrier += -ca.log(yk - ymin + epsilon)  # Barrier for bottom edge
+        barrier += -ca.log(ymax - yk + epsilon)  # Barrier for top edge
 
     # Obstacle signed distance field
-    sdf = SignedDistanceField("./configs/world.yaml", gamma)
-    barrier = 0
-    for k in range(N-1):
-        barrier += sdf.barrier_func(X[k, :])
+    # sdf = SignedDistanceField("./configs/world.yaml", gamma)
+    # barrier = 0
+    # for k in range(N-1):
+    #     barrier += sdf.barrier_func(X[k, :])
 
     # Cost function on input
     cost = 0
